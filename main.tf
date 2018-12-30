822e3f0929aa7a5663bfc56d99dea2da54fcbc64
will Farrell <willfarrell@users.noreply.github.com>
2018-12-30 16:58:19 -0700

    fix: remove region

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 resource "aws_s3_bucket" "main" {
   bucket              = "${var.name}-logs"
-  region              = "${var.region}"
   acl                 = "log-delivery-write"
 
   versioning {
