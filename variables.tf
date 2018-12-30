269dc2764d201b54f8e91d785d94000656fc1380
will Farrell <willfarrell@users.noreply.github.com>
2018-12-30 16:49:52 -0700

    feat: add in default region

--- variables.tf
+++ variables.tf
@@ -3,6 +3,10 @@ variable "tags" {
   type = "map"
   default = {}
 }
+variable "region" {
+  type = "string"
+  default = "us-east-1"
+}
 variable "transition_infrequent_days" {
   default = 30
   # cost is in multiple of 30d
